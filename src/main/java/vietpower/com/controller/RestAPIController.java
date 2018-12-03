598c7b1d10bd4739b48c5b469edc68ca32b7dce2
buitahau@gmail.com <buitahau@gmail.com>
2018-12-04 06:39:13 +0700

    add api get base

--- src/main/java/vietpower/com/controller/RestAPIController.java
+++ src/main/java/vietpower/com/controller/RestAPIController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import vietpower.com.model.Base;
 import vietpower.com.model.Formula;
+import vietpower.com.service.BaseService;
 import vietpower.com.service.FormulaService;
 
 import java.io.Serializable;
@@ -18,11 +20,20 @@ import java.util.List;
 public class RestAPIController implements Serializable{
     @Autowired
     FormulaService formulaService;
+    @Autowired
+    BaseService baseService;
 
     @RequestMapping(value = "/server/api/getAllFormula", method = RequestMethod.GET)
     @ResponseBody
     public List getAllFormula(){
         List<Formula> formulars = formulaService.findAll();
         return formulars;
+    }
+
+    @RequestMapping(value = "/server/api/getAllBase", method = RequestMethod.GET)
+    @ResponseBody
+    public List getAllBase(){
+        List<Base> bases = baseService.findAll();
+        return bases;
     }
 }
